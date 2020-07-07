48b74a94d378af59848e1694f8444a984ccdb8cf
shubhamshelke307 <shubhamshelke307@gmail.com>
2020-07-07 21:17:38 +0530

    add humidity percentage

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request=  require('request')
 const forecast=(lt,lg,callback)=>{
 
-const url='http://api.weatherstack.com/current?access_key=c86dc854442f13bd68ab61de116f6acb&query='+lg+','+lt+'&units=s'
+const url='http://api.weatherstack.com/current?access_key=c86dc854442f13bd68ab61de116f6acb&query='+lg+','+lt+'&units=f'
 
 
 request({url,json:true},(error,{body})=>{
@@ -14,7 +14,7 @@ request({url,json:true},(error,{body})=>{
     }
     else{
         callback(undefined,{
-            forecastdata:'It is currently '+ body.current.temperature+' Degree out. There is a '+ body.current.feelslike +' % chance of rain'
+            forecastdata:'It is currently '+ body.current.temperature+' Degree out. There is a '+ body.current.feelslike +' % chance of rain. Humidity is '+ body.current.humidity+'%'
         })
     }
 })
